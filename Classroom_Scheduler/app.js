4e67ff3ff7b35a684bb71d382ef773b1c24c2afb
TJ Sass <tsass@unomaha.edu>
2015-04-16 22:29:34 -0500

    Changed from jade to html
    changed app.js to only use html files.  jade was nice and has its uses,
    but it is really cryptic.  Only files in html folder are pushed to the
    server to run.

--- Classroom_Scheduler/app.js
+++ Classroom_Scheduler/app.js
@@ -1,31 +1,9 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var upload = require('./routes/upload');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//this loads all files from html folder to server
+app.use('/', express.static( __dirname+ '/html'));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-app.use('/upload',upload);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
