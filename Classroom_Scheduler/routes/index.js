f275004888d32a791d2c12e1f5ed975cdbe761cf
Nick Boeckman <nicholas.boeckman@gmail.com>
2015-04-21 15:26:22 -0500

    Navbar Header
    This creates the header for the pages, and adds the routes for each of
    the pages.

--- Classroom_Scheduler/routes/index.js
+++ Classroom_Scheduler/routes/index.js
@@ -5,10 +5,30 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
 /* GET Upload Page. */
 router.get('/upload', function(req, res, next) {
   res.render('upload', { title: 'Express' });
 });
 
+/* GET Calendar Page. */
+router.get('/calendar', function(req, res, next) {
+  res.render('calendar', { title: 'Express' });
+});
+
+/* GET Preferences Page. */
+router.get('/prefs', function(req, res, next) {
+  res.render('prefs', { title: 'Express' });
+});
+
+/* GET Classroom Page. */
+router.get('/rooms', function(req, res, next) {
+  res.render('rooms', { title: 'Express' });
+});
+
+/* GET Classes Page. */
+router.get('/classes', function(req, res, next) {
+  res.render('classes', { title: 'Express' });
+});
 
 module.exports = router;
