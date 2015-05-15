90d65327fe4ddbf63c28523db408606f2836dd68
TJ Sass <tsass@unomaha.edu>
2015-05-15 02:46:34 -0500

    Over halfway through dbtransactions
    Getting more functions and classes documented

--- out/api.js
+++ out/api.js
@@ -1,6 +1,7 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "DB_Transactions",
         "downloadSchedule",
         "generateSchedule",
         "index.js",
